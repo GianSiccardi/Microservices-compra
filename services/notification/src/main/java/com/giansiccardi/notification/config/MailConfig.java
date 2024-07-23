6bf173dab8240be05f3d49d45aeefa4a1b1eaa4a
GianSiccardi <giansicca70@gmail.com>
2024-07-22 22:15:26 -0300

    cambios

--- services/notification/src/main/java/com/giansiccardi/notification/config/MailConfig.java
+++ services/notification/src/main/java/com/giansiccardi/notification/config/MailConfig.java
@@ -13,8 +13,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("giansicca70@gmail.com");
-        mailSender.setPassword("tadeo123");
+        mailSender.setUsername("mail@prueba");
+        mailSender.setPassword("*");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
